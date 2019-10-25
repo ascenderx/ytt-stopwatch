2001d186ef5543522626a0b387ab4208f35d7c5c
James Downer <downer.james.256k@gmail.com>
2019-10-25 12:36:34 -0600

    Add arc drawing; remodularize labels

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 (() => {
   let cvs;
   let ctx;
-  let lblTime;
+  let lblHours;
+  let lblMinutes;
+  let lblSeconds;
+  let lblMilliseconds;
   let stopwatch;
   const INTERVAL = 10;
+  const START_ANGLE = -Math.PI * 0.5;
+  const ARC_THICKNESS = 10;
   
   function padZerosLeft(num, width) {
     let result = `${num}`.split('');
@@ -16,7 +21,7 @@
     return result.join('');
   }
   
-  function tick() {
+  function update() {
     stopwatch.update();
     
     let hours = stopwatch.hours;
@@ -29,17 +34,62 @@
     let sc = padZerosLeft(seconds, 2);
     let ml = padZerosLeft(milliseconds, 1);
     
-    lblTime.innerText = `${hr}:${mn}:${sc}.${ml}`;
+    lblHours.innerText = hr;
+    lblMinutes.innerText = mn;
+    lblSeconds.innerText = sc;
+    lblMilliseconds.innerText = ml;
+  }
+  
+  function getElapsedAngle() {
+    return 2 * Math.PI * (stopwatch.seconds / 60);
   }
   
   function draw() {
+    ctx.lineWidth = ARC_THICKNESS;
     
+    if (stopwatch.seconds === 0) {
+      ctx.clearRect(0, 0, cvs.width, cvs.height);
+      
+      // draw a black circle
+      ctx.strokeStyle = 'black';
+      ctx.beginPath();
+      ctx.arc(
+        cvs.width * 0.5,
+        cvs.height * 0.5,
+        cvs.width * 0.5 - ARC_THICKNESS,
+        0,
+        2 * Math.PI
+      );
+      ctx.stroke();
+    } else {
+      // draw a green arc
+      ctx.strokeStyle = '#0f0';
+      ctx.beginPath();
+      ctx.arc(
+        cvs.width * 0.5,
+        cvs.height * 0.5,
+        cvs.width * 0.5 - ARC_THICKNESS,
+        START_ANGLE,
+        START_ANGLE + getElapsedAngle()
+      );
+      ctx.stroke();
+    }
+  }
+  
+  function tick() {
+    update();
+    draw();
   }
   
   window.addEventListener('load', () => {
     cvs = document.getElementById('cvs');
     ctx = cvs.getContext('2d');
-    lblTime = document.getElementById('lbl-time');
+    
+    lblHours = document.getElementById('lbl-hours');
+    lblMinutes = document.getElementById('lbl-minutes');
+    lblSeconds = document.getElementById('lbl-seconds');
+    lblMilliseconds = document.getElementById('lbl-milliseconds');
+    
     stopwatch = new Stopwatch();
     animator = new Animator(tick, INTERVAL);
     
